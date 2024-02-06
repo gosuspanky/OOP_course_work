4c4f8cabb953a02ab7235edccffb908590010f90
Artur Gaetov <agaetov@gmail.com>
2024-02-06 20:54:54 +0300

    some changes for searching and saving data (temporally)

--- src/hh_data.py
+++ src/hh_data.py
@@ -8,12 +8,12 @@ from config import DATA_DIR, SORTED_DATA_DIR
 class AbstractAPI(ABC):
 
     @abstractmethod
-    def get_data(self):
+    def get_vacancies(self):
         pass
 
-    @abstractmethod
-    def save_data(self):
-        pass
+    # @abstractmethod
+    # def save_data(self):
+    #     pass
 
 
 class HeadHunterData(AbstractAPI):
@@ -24,7 +24,9 @@ class HeadHunterData(AbstractAPI):
         self.all = []
         self.sorted_list = []
 
-    def get_data(self):
+        self.reworked_dicts = []
+
+    def get_vacancies(self):
         self.all.clear()
         params = {
             "text": f'NAME:{self.vacancy}',
@@ -33,10 +35,24 @@ class HeadHunterData(AbstractAPI):
         }
 
         data = requests.get(f'https://api.hh.ru/vacancies', params)
+        print(data)
 
         self.all.extend(json.loads(data.text)['items'])
 
         return self.all
+
+    def save_vacancies(self):
+        data = self.get_vacancies()
+        json_data = json.dumps(data, ensure_ascii=False)
+
+        with open(DATA_DIR, 'w', encoding='utf-8') as file:
+            file.write(json_data)
+
+    def save_sorted_vacancies(self):
+
+        json_data = json.dumps(self.sorted_list, ensure_ascii=False)
+        with open(SORTED_DATA_DIR, 'w', encoding='utf-8') as file:
+            file.write(json_data)
 
     def sort_by_area(self, area: str):
         self.sorted_list.clear()
@@ -46,26 +62,16 @@ class HeadHunterData(AbstractAPI):
                 self.sorted_list.append(self.all[i])
         return self.sorted_list
 
-    def save_data(self):
-        data = self.get_data()
-        json_data = json.dumps(data, ensure_ascii=False)
-
-        with open(DATA_DIR, 'w', encoding='utf-8') as file:
-            file.write(json_data)
-
-    def save_sorted_data(self):
-
-        json_data = json.dumps(self.sorted_list, ensure_ascii=False)
-        with open(SORTED_DATA_DIR, 'w', encoding='utf-8') as file:
-            file.write(json_data)
 
 
 if __name__ == '__main__':
-    user_vacancy = input('Введите название вакансии или ключевое слово для поиска вакансий\n')
-    hh_api = HeadHunterData(user_vacancy)
-    hh_api.save_data()
+    # user_vacancy = input('Введите название вакансии или ключевое слово для поиска вакансий\n')
+    hh_api = HeadHunterData('Python')
+    hh_api.save_vacancies()
 
-    user_area = input('Введите город в котором ищите вакансию\n').title()
-    hh_api.sort_by_area(user_area)
-    hh_api.save_sorted_data()
-    print(hh_api.sorted_list)
+    # user_area = input('Введите город в котором ищите вакансию\n').title()
+    # hh_api.sort_by_area(user_area)
+    # hh_api.save_sorted_data()
+    # hh_api.filter_for_vacancies()
+    #
+    # print(hh_api.reworked_dicts)
--- tests/test_hh_data.py
+++ tests/test_hh_data.py
@@ -10,13 +10,13 @@ def test_issubclass():
 
 def test_data():
     hh_api = HeadHunterData("Python")
-    hh_api.get_data()
+    hh_api.get_vacancies()
     assert len(hh_api.all) > 0
 
 
 def test_sorted_data():
     v = HeadHunterData("Python")
-    v.get_data()
+    v.get_vacancies()
     v.sort_by_area('Казань')
 
     assert len(v.sorted_list) > 0
