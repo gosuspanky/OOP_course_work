b2b785d4624327c0971df2fae348d88ddd362d02
Artur Gaetov <agaetov@gmail.com>
2024-02-06 18:47:10 +0300

    fix config

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os.path
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_DIR = os.path.join(ROOT_DIR, 'data', 'data.json')
+SORTED_DATA_DIR = os.path.join(ROOT_DIR, 'data', "sorted_data.json")
