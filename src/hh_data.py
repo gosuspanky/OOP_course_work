3fb7edd028cf12be1ce42a480787a970ccbefdba
Artur Gaetov <agaetov@gmail.com>
2024-02-06 18:46:47 +0300

    added new filter for vacancies

--- src/hh_data.py
+++ src/hh_data.py
@@ -2,7 +2,7 @@ import json
 from abc import ABC, abstractmethod
 import requests
 
-from config import DATA_DIR
+from config import DATA_DIR, SORTED_DATA_DIR
 
 
 class AbstractAPI(ABC):
@@ -18,21 +18,33 @@ class AbstractAPI(ABC):
 
 class HeadHunterData(AbstractAPI):
 
-    def __init__(self, user_vacancy):
-        self.user_vacancy = user_vacancy
+    def __init__(self, vacancy):
+        self.vacancy = vacancy
+
         self.all = []
+        self.sorted_list = []
 
     def get_data(self):
         self.all.clear()
         params = {
-            "text": f'NAME:{self.user_vacancy}',
-            "name": "Россия"
+            "text": f'NAME:{self.vacancy}',
+            "area": 113,
+            'per_page': 100
         }
 
         data = requests.get(f'https://api.hh.ru/vacancies', params)
 
         self.all.extend(json.loads(data.text)['items'])
+
         return self.all
+
+    def sort_by_area(self, area: str):
+        self.sorted_list.clear()
+
+        for i in range(len(self.all)):
+            if self.all[i]['area']['name'] == area:
+                self.sorted_list.append(self.all[i])
+        return self.sorted_list
 
     def save_data(self):
         data = self.get_data()
@@ -41,7 +53,19 @@ class HeadHunterData(AbstractAPI):
         with open(DATA_DIR, 'w', encoding='utf-8') as file:
             file.write(json_data)
 
+    def save_sorted_data(self):
+
+        json_data = json.dumps(self.sorted_list, ensure_ascii=False)
+        with open(SORTED_DATA_DIR, 'w', encoding='utf-8') as file:
+            file.write(json_data)
+
 
 if __name__ == '__main__':
-    hh_api = HeadHunterData("Python")
+    user_vacancy = input('Введите название вакансии или ключевое слово для поиска вакансий\n')
+    hh_api = HeadHunterData(user_vacancy)
     hh_api.save_data()
+
+    user_area = input('Введите город в котором ищите вакансию\n').title()
+    hh_api.sort_by_area(user_area)
+    hh_api.save_sorted_data()
+    print(hh_api.sorted_list)
