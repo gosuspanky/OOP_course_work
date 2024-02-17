784a48ca25752aed0a5de23f229b15371112fe38
Artur Gaetov <agaetov@gmail.com>
2024-02-17 23:49:38 +0300

    added paths to new files

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_DIR = os.path.join(ROOT_DIR, 'data', 'data.json')
 SORTED_DATA_DIR = os.path.join(ROOT_DIR, 'data', "sorted_data.json")
+
+TXT_OUTPUT_DIR = os.path.join(ROOT_DIR, 'data', "Vacancies.txt")
+JSON_OUTPUT_DIR = os.path.join(ROOT_DIR, 'data', "Vacancies.json")
